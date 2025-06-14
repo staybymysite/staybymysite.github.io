e72fc61091b225633369404350f1642f9b0a9880
staybymysite <bbworld830@gmail.com>
2025-06-15 03:00:09 +0800

    improve image loading

--- script.js
+++ script.js
@@ -66,6 +66,13 @@ function easeInOutQuad(t) {
     return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
 }
 
+// 載入base64 slides
+["story_01", "story_02", "story_03"].forEach((slide, index) => {
+    const img = document.getElementById(slide);
+    const base64 = getComputedStyle(document.documentElement).getPropertyValue("--"+slide+"-base64");
+    img.src = base64.slice(4, -1).replaceAll('"', ''); // Remove url("...") wrapper
+})
+
 // slide 滑動
 const radios = Array.from(document.querySelectorAll('input[name="a"]'));
 const slides = document.querySelectorAll('.ci');
