6afcf5231800a524df268fbb89da99eab0b9df67
staybymysite <bbworld830@gmail.com>
2025-06-15 04:39:55 +0800

    clean info

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ function easeInOutQuad(t) {
 // 載入base64 slides
 ["story_01", "story_02", "story_03"].forEach((slide, index) => {
     const img = document.getElementById(slide);
-    const base64 = getComputedStyle(document.documentElement).getPropertyValue("--"+slide+"-base64");
+    const base64 = getComputedStyle(document.documentElement).getPropertyValue("--" + slide + "-base64");
     img.src = base64.slice(4, -1).replaceAll('"', ''); // Remove url("...") wrapper
 })
 
@@ -99,4 +99,10 @@ slides.forEach((slide, index) => {
             }
         }
     });
+});
+
+// get there toggle
+document.querySelector('.there-title').addEventListener('click', function () {
+    const ul = document.querySelector('.route');
+    ul.classList.toggle('show');
 });
